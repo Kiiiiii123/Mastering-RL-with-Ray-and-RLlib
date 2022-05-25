e95ce080cb9a680d11b1357f37e3b8943e2492e1
Kiiiiii123 <zf1721418@buaa.edu.cn>
2022-05-25 08:38:23 +0800

    Modify some code.

--- Policy-Based Methods/pg_agent.py
+++ Policy-Based Methods/pg_agent.py
@@ -6,23 +6,20 @@ from ray import tune
 from ray.rllib.agents.pg.pg import DEFAULT_CONFIG
 from ray.rllib.agents.pg.pg import PGTrainer as trainer
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--env", default='LunarLanderContinuous-v2', help="Gym environment name.")
+    parser.add_argument("--env", help="Gym env name.")
     args = parser.parse_args()
-
     config = DEFAULT_CONFIG.copy()
     config_update = {
         "env": args.env,
-        "num_gpus": 0,
-        "num_workers": 12,
+        "num_gpus": 1,
+        "num_workers": 50,
         "evaluation_num_workers": 10,
         "evaluation_interval": 1,
     }
     config.update(config_update)
     pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(config)
-
     ray.init()
-    tune.run(trainer, stop={"timesteps_total": 200000}, config=config)
+    tune.run(trainer, stop={"timesteps_total": 2000000}, config=config)
